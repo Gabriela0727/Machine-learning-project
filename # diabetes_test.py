9cf6bdfb889553fe3f1a8d14c9fae1fd0c8382ae
Gabriela Rubio <116653558+Gabriela0727@users.noreply.github.com>
2025-11-19 17:39:34 -0500

    Update # diabetes_test.py

--- # diabetes_test.py
+++ # diabetes_test.py
@@ -417,5 +417,7 @@ if __name__ == "__main__":
  - https://www.kaggle.com/code/shrutimechlearn/step-by-step-diabetes-classification?scriptVersionId=200050106
  - https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database
  - https://pmc.ncbi.nlm.nih.gov/articles/PMC10107388/ 
+ - Role of K-nearest neighbor in detection of Diabetes Mellitus
+ - https://pmc.ncbi.nlm.nih.gov/articles/PMC7381989/
  - GitHub copilot
  """
